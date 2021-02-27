355595c32ae2e47d9f239073025fd40037b9933e
alfred-gorski <dzhongzhu@icloud.com>
2021-02-27 13:43:54 +0100

    feat[basic]: add queue module

---
+++ Inc/U_Queue.h
@@ -0,0 +1,28 @@
+#ifndef U_QUEUE_H
+#define U_QUEUE_H
+
+#include <stdint.h>
+#include <stdio.h>
+
+
+#define QUEUE_LOG2_CAPACITY 6U
+#define QUEUE_CAPACITY (1 << QUEUE_LOG2_CAPACITY)
+#define QUEUE_MASK (QUEUE_CAPACITY - 1)
+
+// first-in-first-out Queue implemented with Ring Buffer
+typedef struct {
+  unsigned wcntr;
+  unsigned rcntr;
+  uint8_t buffer[QUEUE_CAPACITY];
+} Queue;
+
+void init(Queue *const p);
+int isEmpty(const Queue *const p);
+int isFull(const Queue *const p);
+uint8_t pop(Queue *const p);
+void push(Queue *const p, uint8_t data);
+
+
+
+
+#endif
---
+++ Src/U_Queue.c
@@ -0,0 +1,21 @@
+#include "U_Queue.h"
+
+unsigned len(const Queue *const p);
+
+void init(Queue *const p) {
+  p->wcntr = 0;
+  p->rcntr = 0;
+}
+
+int isEmpty(const Queue *const p) { return len(p) == 0; }
+
+int isFull(const Queue *const p) { return len(p) == QUEUE_CAPACITY; }
+
+// TODO: ERRO Handling
+uint8_t pop(Queue *const p) { return p->buffer[p->rcntr++ & QUEUE_MASK]; }
+// TODO: ERRO Handling
+void push(Queue *const p, uint8_t data) {
+  p->buffer[p->wcntr++ & QUEUE_MASK] = data;
+}
+
+unsigned len(const Queue *const p) { return p->wcntr - p->rcntr; }
